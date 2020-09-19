3bfa5f2b1e8a789fcd902bdc39ed3710f0d886a8
Shusty <shujitsu@gmail.com>
2020-09-19 22:12:26 +0200

    Updates

---
+++ precache-manifest.5f99357b434b808e99ae83ed1a82994c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4ec7ba80f43e67a2a19fb4b2f59e3f",
+    "url": "/react-rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "069e5ea1c49bb2d7d4c0",
+    "url": "/react-rock-paper-scissors/static/css/main.52926b92.chunk.css"
+  },
+  {
+    "revision": "d0425fda450d8d0d93cd",
+    "url": "/react-rock-paper-scissors/static/js/2.530cffd9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-rock-paper-scissors/static/js/2.530cffd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069e5ea1c49bb2d7d4c0",
+    "url": "/react-rock-paper-scissors/static/js/main.4692cb45.chunk.js"
+  },
+  {
+    "revision": "99c35c64927a9b6cf6af",
+    "url": "/react-rock-paper-scissors/static/js/runtime-main.5ecae2e0.js"
+  }
+]);
